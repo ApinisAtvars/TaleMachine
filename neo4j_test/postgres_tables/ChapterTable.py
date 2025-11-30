ef302b3fda6f66a079400eb3a401e9ee1f557795
Violetta Nguyen <violetta.nguyen@student.howest.be>
2025-11-30 16:02:23 +0100

    DB: role removed

--- neo4j_test/postgres_tables/ChapterTable.py
+++ neo4j_test/postgres_tables/ChapterTable.py
@@ -8,7 +8,6 @@ class ChapterTable(Base): # Chapters table
     __tablename__ = 'chapters'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     content = Column(Text)
-    role = Column(String)  # 'user' or 'ai'
     timestamp = Column(Integer)  # Unix timestamp
     story_id = Column(Integer, ForeignKey('stories.id', ondelete="CASCADE"))  # Foreign key to stories table
     story = relationship("StoryTable")
