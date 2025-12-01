a09282c56854b38090b7dd7ad28599213eb3d50d
Violetta Nguyen <violetta.nguyen@student.howest.be>
2025-12-01 14:53:59 +0100

    FEAT: images and chapter routers

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ import uvicorn
 
 from routes.StoryRoute import story_router
 from routes.MessagesRoute import messages_router
+from routes.ImagesRouter import images_router
+from routes.ChapterRouter import chapter_router
 from services.postgres_service import PostgresService
 
 @asynccontextmanager
@@ -23,6 +25,8 @@ async def root():
 # Include routers
 app.include_router(story_router)
 app.include_router(messages_router)
+app.include_router(images_router)
+app.include_router(chapter_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, port=7890)
