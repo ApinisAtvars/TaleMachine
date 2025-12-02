f3a4c27ad69977504cb88eb1ad7dcf9c8d736a12
ApinisAtvars <atvars.apinis@student.howest.be>
2025-12-02 16:05:32 +0100

    WIP: CORS for FastAPI

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routes.StoryRoute import story_router
@@ -17,6 +18,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def root():
